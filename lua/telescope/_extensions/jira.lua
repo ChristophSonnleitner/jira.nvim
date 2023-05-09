ba82cee62f049768c82a44e82e0fe6bef0c01c45
Chris <christoph.sonnleitner@outlook.com>
2023-05-09 21:02:20 +0200

    lkjdkfj

--- lua/telescope/_extensions/jira.lua
+++ lua/telescope/_extensions/jira.lua
@@ -123,10 +123,10 @@ local live_grep_files = function(opts)
         end
         
         local search_file_content = flatten {{"rg", "--color=never", "--with-filename", "-l",  "---hidden", "--follow"}, prompt, search_list}
-        local search_file_name = flatten {{"rg", "--color=never", "--files", "--hidden", "--follow", "--glob", "'!.git'","|", "rg", "--color=never", "-l"}, prompt, search_list}
+        local search_file_name = flatten {{"rg", "--color=never", "--files", "--hidden", "--follow","|", "rg", "--color=never", "-l"}, prompt, search_list}
         local search_command = flatten {search_file_content , ";", search_file_name}
          -- return flatten { { "rg", "--color=never", "--no-heading", "--with-filename", "--line-number", "--column", "--smart-case", "-l"}, "--", prompt, search_list }
-        return search_file_content
+        return search_file_name
     end
 
     pickers
