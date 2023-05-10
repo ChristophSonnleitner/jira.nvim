7e47a3d50b4ddcfedcd7ef1237ddbb2021972441
Chris <christoph.sonnleitner@outlook.com>
2023-05-10 19:57:14 +0200

    asdadf

--- lua/telescope/_extensions/lib.lua
+++ lua/telescope/_extensions/lib.lua
@@ -1,46 +1,45 @@
 
-local actions = require('telescope.actions')
-local finders = require('telescope.finders')
 local pickers = require('telescope.pickers')
+local finders = require('telescope.finders')
 local sorters = require('telescope.sorters')
-local conf = require('telescope.config').values
+local actions = require('telescope.actions')
 
-local function word_search_picker(opts)
-  opts = opts or {}
+local function word_search_picker(prompt)
+  local word_list = {}
+  for word in string.gmatch(prompt, "%S+") do
+    table.insert(word_list, word)
+  end
 
-  pickers.new(opts, {
+  local grep_string = table.concat(word_list, "\\|")
+
+  pickers.new({}, {
     prompt_title = 'Word Search',
-    finder = finders.new_job(function(prompt)
-      if not prompt or prompt == "" then return end
-
-      -- Split the prompt into words
-      local words = {}
-      for word in string.gmatch(prompt, "%S+") do
-        table.insert(words, word)
-      end
-
-      local rg = vim.fn.executable("rg") == 1
-
-      -- Check if the word is in the file or in the filename.
-      for _, word in ipairs(words) do
-        if rg then
-          return { "rg", "--files", "--no-ignore", "--hidden", "-g", "*" .. word .. "*" }
-        else
-          return { "find", ".", "-type", "f", "-iname", "*" .. word .. "*" }
-        end
-      end
-    end),
-    sorter = conf.generic_sorter(opts),
-    attach_mappings = function(prompt_bufnr, map)
-      actions.select_default:replace(function()
-        local selection = actions.get_selected_entry()
-        actions.close(prompt_bufnr)
-        local filename = selection.value:match("^(.-):")
-        vim.cmd('edit ' .. filename)
-      end)
-
+    finder = finders.new_oneshot_job(
+      { "grep", "-ril", grep_string },
+      {
+        cwd = ".",
+        entry_maker = function(line)
+          local count = 0
+          for _, word in ipairs(word_list) do
+            if string.find(line, word) then
+              count = count + 1
+            end
+          end
+          return {
+            valid = true,
+            value = line,
+            ordinal = count .. line,
+            display = count .. " : " .. line,
+          }
+        end,
+      }
+    ),
+    sorter = sorters.get_fzy_sorter(),
+    attach_mappings = function(_, map)
+      map('i', '<CR>', actions.select_default)
+      map('n', '<CR>', actions.select_default)
       return true
-    end,
+    end
   }):find()
 end
 
